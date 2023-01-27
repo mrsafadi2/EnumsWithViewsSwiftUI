289f708b3c32eb649c7bdb77863d99e2b405ed22
mrsafadi2 <mrsafadi2@gmail.com>
2023-01-27 12:52:20 +0200

    Initial Commit

---
+++ EnumsWithViews/EnumsWithViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnumsWithViewsApp.swift
+//  EnumsWithViews
+//
+//  Created by Mohammed Safadi Macbook Pro on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct EnumsWithViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
